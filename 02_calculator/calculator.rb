df05d5b8f7a52210672ce1a4a001425be2b7fc75
John Skender <jnskender@gmail.com>
2017-02-08 15:28:26 -0600

    02_calculator passes

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add num1,num2
+  "#{num1}".to_f + "#{num2}".to_f
+end
+
+def subtract num1,num2
+  "#{num1}".to_f - "#{num2}".to_f
+end
+
+def sum array
+  sum = 0
+  for i in 0..array.length
+    sum += array[i].to_i
+  end
+  sum
+end
