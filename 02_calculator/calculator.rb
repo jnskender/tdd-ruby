e73a477cab3857a274bfc050266f3de9f9769b68
John Skender <jnskender@gmail.com>
2017-02-08 16:51:07 -0600

    02_calculator passes and extra credit testing works

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -14,3 +14,27 @@ def sum array
   end
   sum
 end
+
+def multiply *numbers
+  result = 1
+  numbers.each {|n| result = result * n }
+  result
+end
+
+def power num1,num2
+  ("#{num1}".to_i) ** ("#{num2}".to_i)
+end
+
+def factorial number
+  array = []
+  for i in 0..(number - 1)
+    array.push(i + 1) #exclude zero from multiplication list
+  end
+  if array.empty?
+    1
+  elsif array.length == 1
+    1
+  else
+     array.inject(:*) #multiply entire array
+  end
+end
