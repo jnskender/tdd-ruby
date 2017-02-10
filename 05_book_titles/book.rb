4cf462321586a8b0ac29e9d447895997c8727f4f
John Skender <jnskender@gmail.com>
2017-02-10 15:26:38 -0600

    05_book_titles passes

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
-# write your code here
+    def title=(title)
+        @title = titleize(title)
+    end
+
+    attr_reader :title
+
+    def titleize(string)
+        words = string.to_s.split(/\W+/)
+        lowercase = %w(an a and the but over in of)
+        if words.size <= 1
+            words[0].capitalize
+        else
+            words[0] = words[0].capitalize # Always capitalize first word
+            for i in 1..words.size - 1
+                words[i] = words[i].capitalize unless lowercase.include?(words[i])
+            end
+            words.join(' ')
+        end
+    end
 end
