c475c4fdfd861441b6bed4c499546cf93ae366e1
John Skender <jnskender@gmail.com>
2017-02-10 17:44:23 -0600

    06_timer passes

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,24 @@
 class Timer
-  #write your code here
+    attr_accessor:seconds
+
+    def time_string=(seconds)
+        @time_string = timer(@seconds)
+    end
+
+    def initialize
+      @seconds = 0
+    end
+
+    def time_string
+      time_in_seconds = @seconds
+      hours = time_in_seconds / 3600
+      minutes = (time_in_seconds / 60) - (60 * hours)
+      seconds = time_in_seconds - (minutes * 60) - (3600 * hours)
+
+      hours = '0' + hours.to_s if hours < 10
+      minutes = '0' + minutes.to_s if minutes < 10
+      seconds = '0' + seconds.to_s if seconds < 10
+      hours.to_s + ':' + minutes.to_s + ':' + seconds.to_s
+    end
+
 end
